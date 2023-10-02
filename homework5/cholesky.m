6a91756336a82c383fd90805c8049331a1fef053
Marvyn B <55770960+MarvynBailly@users.noreply.github.com>
2023-10-02 13:41:34 -0700

    Update cholesky.m

--- homework5/cholesky.m
+++ homework5/cholesky.m
@@ -1,15 +1,17 @@
 function result = cholesky(A)
-    [n,~] = size(A);
-    L = A;
+    n = length(A);
+    L = zeros(n,n);
 
     % Check if A is SPD
     for j = 1 : n
         L(j,j) = (A(j,j) - sum(L(j,1:j-1).^2))^(1/2); 
+
         if(L(j,j) == 0 || ~isreal(L(j,j)))
             fprintf("The matrix is not positive definite")
             result = 0;
             return 
         end
+       
         for i = j + 1 : n
             L(i,j) = (A(i,j) - sum(L(i,1:j-1).*L(j,1:j-1)))/L(j,j); 
         end
