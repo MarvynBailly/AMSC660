b3d5f7c333c7c894e1bf422c3da36a2b89cb91c4
Marvyn B <marvyn@marvyn.com>
2023-10-27 19:15:50 -0400

    updated code

--- homework8/NestedDissection_anysize.m
+++ homework8/NestedDissection_anysize.m
@@ -31,6 +31,10 @@ b = btemp;
 
 sol = A\b;
 
+
+figure(1000)
+spy(A)
+
 %% Nested Dissection
 % Nested Dissection algorithm solves Ax=b
 % It rearranges A using a permutation matrix P that it constructs
@@ -71,12 +75,12 @@ if m ~= n
 end
 % if level is even do vertical split
 % if level is odd do horizontal split
-N_grid = nx_grid*ny_grid; % the grid size
-par = mod(level,2); % parity
+N_grid = nx_grid*ny_grid; % the grid size              % one flop here
+par = mod(level,2); % parity                           % one flop here
 switch par
     case 0 % vertical split
         if nx_grid >= 3
-            nx_Omega1 = floor(nx_grid/2); % # of columns in Omega1
+            nx_Omega1 = floor(nx_grid/2); % # of columns in Omega1  
             N_Omega1 = nx_Omega1*ny_grid; % |Omega1| 
             ind_Omega3 = N_Omega1 + 1 : N_Omega1 + ny_grid; % indices of Omega3
             N_Omega3 = length(ind_Omega3);
@@ -122,12 +126,11 @@ switch par
         fprintf('Error: par = %d\n',par);
         return
 end
-% fprintf('size(A) = [%d,%d], nxy = %d, N_Omega1 = %d, N_Omega2 = %d, N_Omega3 = %d\n',...
-%     size(A,1),size(A,2),ny_grid,N_grid,N_Omega1,N_Omega2,N_Omega3);
-% fprintf('ind_Omega2(1) = %d, ind_Omega2(end) = %d\n',ind_Omega2(1),ind_Omega2(end));
+%fprintf('size(A) = [%d,%d], nxy = %d, N_Omega1 = %d, N_Omega2 = %d, N_Omega3 = %d\n',size(A,1),size(A,2),ny_grid,N_grid,N_Omega1,N_Omega2,N_Omega3);
+%fprintf('ind_Omega2(1) = %d, ind_Omega2(end) = %d\n',ind_Omega2(1),ind_Omega2(end));
+
 A11 = A(ind_Omega1,ind_Omega1);
 A22 = A(ind_Omega2,ind_Omega2);
-
 [L11,U11,P11,~] = MyDissection(A11,nx_Omega1,ny_Omega1,level + 1);
 [L22,U22,P22,~] = MyDissection(A22,nx_Omega2,ny_Omega2,level + 1);
 
@@ -142,9 +145,11 @@ P = P*P1;
 A = P*A0*P';
 % extract nonzero blocks of A
 A11 = A(1 : N_Omega1,1 : N_Omega1);
+
 istart2 = N_Omega1 + 1;
 ifinish2 = N_Omega1 + N_Omega2;
 istart3 = ifinish2 + 1;
+
 A22 = A(istart2 : ifinish2,istart2 : ifinish2);
 A13 = A(1 : N_Omega1,istart3 : end);
 A23 = A(istart2 : ifinish2,istart3 : end);
@@ -152,17 +157,44 @@ A31 = A(istart3 : end,1 : N_Omega1);
 A32 = A(istart3 : end,istart2 : ifinish2);
 A33 = A(istart3 : end,istart3 : end);
 % compute the Schur complement
+%disp("----Computing the Schur Complement----")
+fprintf('Size of A31 %dx%d Size of U11 and L11 %dx%d.\n',size(A33,1),size(A33,2),size(U11,1),size(U11,2))
+fprintf('Size of A32 %dx%d Size of U22 and L22 %dx%d.\n',size(A32,1),size(A32,2),size(U22,1),size(U22,2))
+
+figure(3)
+spy(L11)
+title(sprintf('Sparsity pattern of L11'));
+figure(4)
+spy(U11)
+title(sprintf('Sparsity pattern of U11'));
+figure(5)
+spy(A13)
+title(sprintf('Sparsity pattern of A13'));
+figure(6)
+spy(A31)
+title(sprintf('Sparsity pattern of A31'));
+figure(7)
+spy(L22)
+title(sprintf('Sparsity pattern of L22'));
+figure(8)
+spy(U22)
+title(sprintf('Sparsity pattern of U22'));
+figure(9)
+spy(A23)
+title(sprintf('Sparsity pattern of A23'));
+figure(10)
+spy(A32)
+title(sprintf('Sparsity pattern of A32'));
 
 S33 = A33 - A31*(U11\(L11\A13)) - A32*(U22\(L22\A23));
-%S33 = S33 + 1e-15*eye(size(S33));
+
 
 
 % compute LU factorization of S33
-%[L33,U33] = lu(S33);
+%[L33,U33] =     (S33);
 R33 = chol(S33);
 U33 = R33;
 L33 = R33';
-
 
 % form the LU decomposition of A
 L = sparse(N_grid,N_grid);
@@ -182,72 +214,7 @@ U(istart2 : ifinish2,istart3 : end) = L22\A23;
 % fprintf('norm(full(A - L*U)) = %d\n',norm(full(A - L*U)));
 end
 
-%% make the matrix, the right-hand side, and the exact solution for 
-function [A,b,sol] = TestMatrixA(n)
-% Solving PDE -grad * (a(x,y) * grad(u)) = f(x,y)
-% in the domain D = [0,1]^2 discretized to an n-by-n grid
-% Boundary condition: u = 0 on the boundary of D
-n1 = n - 1;
-h = 1/n1;
-Iinner = 2 : n - 1;
 
-t = linspace(0,1,n);
-[x,y] = meshgrid(t,t);
-% set f(x,y) so that the exact solution is the given one 
-uexact = x.*(1-x).*y.*(1-y); % u exact
-a = 1 + x + 2*y.^2;
-f = diff_oper(h,uexact,a);
-
-A = make_matrix(a,h);
-b = f(:);
-
-uinner = uexact(Iinner,Iinner);
-sol = uinner(:);
-end
-%%
-function Lu = diff_oper(h,u,a)
-n = length(u);
-
-    as = 0.5*(a + circshift(a,[1,0]))/h^2;
-    an = 0.5*(a + circshift(a,[-1,0]))/h^2;
-    aw = 0.5*(a + circshift(a,[0,1]))/h^2;
-    ae = 0.5*(a + circshift(a,[0,-1]))/h^2;
-    ap = aw + ae + as + an;
-    I = 2 : n - 1;
-
-Lu = ap(I,I).*u(I,I) - as(I,I).*u(I - 1,I) ...
-    - an(I,I).*u(I + 1,I) - aw(I,I).*u(I,I - 1)...
-    - ae(I,I).*u(I,I + 1);
-end
-
-%%
-function A = make_matrix(a,h)
-h2 = h^2;
-n = length(a);
-n2 = n - 2;
-cN = 0.5*(a + circshift(a,[-1 0]))/h2;
-cS = 0.5*(a + circshift(a,[1 0]))/h2;
-cW = 0.5*(a + circshift(a,[0 1]))/h2;
-cE = 0.5*(a + circshift(a,[0 -1]))/h2;
-cP = -(cE + cW + cN + cS);
-Iinner = 2 : n - 1;
-an = -cN(Iinner,Iinner);
-as = -cS(Iinner,Iinner);
-aw = -cW(Iinner,Iinner);
-ae = -cE(Iinner,Iinner);
-ap = -(an + as + ae + aw);
-% Conversion to column vectors
-cn = an(:); cs = as(:); cw = aw(:); ce = ae(:); cp = ap(:);
-
-% First set up the mathrix A without taking care of B and D
-A = spdiags([circshift(cw,[-n2 0]),circshift(cs,[-1 0]),cp,...
-    circshift(cn,[1 0]),circshift(ce,[n2 0])],[-n2,-1,0,1,n2],n2*n2,n2*n2);
-% Take care of the Dirichlet BCs: u_y(x,0) = u_y(x,1) = 0
-for j = 1 : n2 - 1 
-    A(n2*j,n2*j + 1) = 0;
-    A(n2*j + 1,n2*j) = 0;
-end
-end
 
         
         
